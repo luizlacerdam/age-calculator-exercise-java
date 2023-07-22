368c590582b63402da112533413b9340eba52fab
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-07-22 18:03:42 -0300

    :sparkles: [feat] service-validateSyntax

--- src/main/java/com/betrybe/calcuradoradeidade/service/AgeCalculatorService.java
+++ src/main/java/com/betrybe/calcuradoradeidade/service/AgeCalculatorService.java
@@ -1,6 +1,7 @@
 package com.betrybe.calcuradoradeidade.service;
 
 import com.betrybe.calcuradoradeidade.exception.FutureDateException;
+import com.betrybe.calcuradoradeidade.exception.InvalidSyntaxDateException;
 import java.time.LocalDate;
 import java.time.Period;
 import org.springframework.stereotype.Service;
@@ -14,12 +15,25 @@ public class AgeCalculatorService {
   public int calculateAge(String dateString) {
     LocalDate dataInicial = LocalDate.parse(dateString);
     LocalDate dataFinal = LocalDate.now();
-
+    validateSyntax(dateString);
     Period intervalo = Period.between(dataInicial, dataFinal);
     if (intervalo.getYears() < 0) {
       throw new FutureDateException("This is a future date.");
     }
     return intervalo.getYears();
+  }
+
+  private void validateSyntax(String date) throws InvalidSyntaxDateException {
+    String[] dateBlocks = date.split("-");
+    if (dateBlocks.length != 3) {
+      throw new InvalidSyntaxDateException("Invalid date format. Expected aa-mm-dd.");
+    }
+    String year = dateBlocks[0];
+    String month = dateBlocks[1];
+    String day = dateBlocks[2];
+    if (year.length() != 4 || month.length() != 2 || day.length() != 2) {
+      throw new InvalidSyntaxDateException("Invalid date format. Expected aa-mm-dd.");
+    }
   }
 
   public int calculateAgeWithDefault(String date, int defaultAge) {
