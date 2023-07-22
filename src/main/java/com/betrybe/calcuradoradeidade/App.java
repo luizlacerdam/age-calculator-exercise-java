acf6fb790c15e246484c2a7a42b1265f676497aa
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-07-22 10:09:28 -0300

    :sparkles: [feat] service-calcuateAge

--- src/main/java/com/betrybe/calcuradoradeidade/App.java
+++ src/main/java/com/betrybe/calcuradoradeidade/App.java
@@ -1,5 +1,6 @@
 package com.betrybe.calcuradoradeidade;
 
+import com.betrybe.calcuradoradeidade.service.AgeCalculatorService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -13,6 +14,8 @@ public class App {
    * main method.
    */
   public static void main(String[] args) {
-    SpringApplication.run(App.class, args);
+    AgeCalculatorService ageCalculatorService = new AgeCalculatorService();
+    System.out.println(ageCalculatorService.calculateAge("2000-03-20"));
+//  SpringApplication.run(App.class, args);
   }
 }
--- src/main/java/com/betrybe/calcuradoradeidade/service/AgeCalculatorService.java
+++ src/main/java/com/betrybe/calcuradoradeidade/service/AgeCalculatorService.java
@@ -1,13 +1,18 @@
 package com.betrybe.calcuradoradeidade.service;
 
+import java.time.LocalDate;
+import java.time.Period;
+
 /**
  * Aqui nessa classe devem ser implementados os métodos para atender aos requisitos do exercício,
  * sinta-se à vontade para criar métodos privados para te auxiliar nas validações.
  */
 public class AgeCalculatorService {
-  public int calculateAge(String date) {
-    // TODO method implementation
-    return -1;
+  public int calculateAge(String dateString) {
+    LocalDate dataInicial = LocalDate.parse(dateString);
+    LocalDate dataFinal = LocalDate.now();
+    Period intervalo = Period.between(dataInicial, dataFinal);
+    return intervalo.getYears();
   }
 
   public int calculateAgeWithDefault(String date, int defaultAge) {
